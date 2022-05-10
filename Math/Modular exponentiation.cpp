a0d82843dd0c7e356f80fdcf3ec73c34c69703d4
niranjan-2024 <89894972+niranjan-2024@users.noreply.github.com>
2022-05-10 23:00:57 +0530

    Update Modular exponentiation.cpp

--- Math/Modular exponentiation.cpp
+++ Math/Modular exponentiation.cpp
@@ -1,33 +1,16 @@
-class Solution{
-public:
-    bool isPrime(int n){
-       if(n<=1){
-           return false;
-       }
-       if(n==2 || n==3){
-           return true;
-       }
-       if(n%2==0 || n%3==0){
-           return false;
-       }
-       for(int i=5;i*i<=n;i=i+6){
-           if(n%i==0 || n%(i+2)==0){
-               return false;
-           }
-       }
-       return true;
-   }
-   
-    long long primeProduct(long long L, long long R){
-        long long res=1;
-        long long m = pow(10,9)+7;
-        for(int i=L;i<=R;i++){
-           if(isPrime(i)){
-               res=res*i;
-               
-           }
-           res=res%m;
-       }
-       return res%m;
-    }
+class Solution
+{
+	public:
+		long long int PowMod(long long int x,long long int n,long long int M)
+		{
+		    int res = 1;
+		    while(n>0){
+		        if(n&1){
+		            res = ( 1LL * (res) * (x)%M)%M;
+		        }
+		        x = (1LL * (x)%M * (x)%M )%M;
+		        n = n>>1;
+		    }
+		    return res;
+		}
 };
