6d426372a68ac57f1818d9a29c04c789a05d54c5
niranjan-2024 <89894972+niranjan-2024@users.noreply.github.com>
2022-05-14 17:54:50 +0530

    Create Adjacency list.cpp

---
+++ Graph/Adjacency list.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    // Function to return the adjacency list for each vertex.
+    vector<vector<int>> printGraph(int V, vector<int> adj[]) {
+        vector<vector<int>> ans(V);
+        for(int i=0;i<V;i++){
+            ans[i].push_back(i);
+            for(auto j : adj[i]){
+                ans[i].push_back(j);
+            }
+        }
+        return ans;
+    }
+};
