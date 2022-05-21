ee5c5e52f02ff470e7827c1973e753898ec9ea2d
niranjan-2024 <89894972+niranjan-2024@users.noreply.github.com>
2022-05-22 01:43:36 +0530

    Create quick sort on singly.cpp

---
+++ Linked List/quick sort on singly.cpp
@@ -0,0 +1,41 @@
+void swapVal(node* first, node* sec)
+{
+    int temp = first->data;
+    first->data = sec->data;
+    sec->data = temp;
+}
+
+node* partition(node* start, node* end)
+{
+    int pivotVal = start->data;
+    node* i = start;
+    node* j = start->next;
+    
+    while(j != end)
+    {
+        if(j->data < pivotVal)
+        {
+            i = i->next;
+            swapVal(i,j);
+        }
+        j = j->next;
+    }
+    swapVal(i, start);
+    return i;
+}
+
+void QuickSort(node* start, node* end)
+{
+    if(start != end)
+    {
+        node* pivot = partition(start, end);
+        QuickSort(start, pivot);
+        QuickSort(pivot->next, end);   
+    }
+}
+
+//you have to complete this function
+void quickSort(struct node **headRef) {
+    
+    QuickSort(*headRef, NULL);
+}
