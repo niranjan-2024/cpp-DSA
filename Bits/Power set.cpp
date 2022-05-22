d82706dc41ef54471e3f3350c6aeed51e0f82a8b
niranjan-2024 <89894972+niranjan-2024@users.noreply.github.com>
2022-05-22 22:40:38 +0530

    Create Power set.cpp

---
+++ Bits/Power set.cpp
@@ -0,0 +1,19 @@
+class Solution{
+	public:
+		vector<string> AllPossibleStrings(string s){
+		    vector<string> ans;
+		    int n = s.length();
+		    
+		    for(int i=1;i<(1<<n);i++){
+		        string str = "";
+		        for(int j=0;j<n;j++){
+		            if(i&(1<<j)){
+		                str += s[j];
+		            }
+		        }
+		        ans.push_back(str);
+		    }
+		    sort(ans.begin(),ans.end());
+		    return ans;
+		}
+};
