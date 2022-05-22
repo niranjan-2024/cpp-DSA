349b4ef7d8f6ff981b33ebed61b2e2f6b695f329
niranjan-2024 <89894972+niranjan-2024@users.noreply.github.com>
2022-05-22 19:10:35 +0530

    Create Number of 1 bits.cpp

---
+++ Bits/Number of 1 bits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int setBits(int N) {
+        int cnt = 0;
+        while(N!=0){
+            if(N&1){
+                cnt++;
+            }
+            N = N>>1;
+        }
+        return cnt;
+    }
+};
