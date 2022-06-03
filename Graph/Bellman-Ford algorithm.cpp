993189defa036830c92c313b3269f77c4758e072
niranjan-2024 <89894972+niranjan-2024@users.noreply.github.com>
2022-06-04 01:25:57 +0530

    Create Bellman-Ford algorithm.cpp

---
+++ Graph/Bellman-Ford algorithm.cpp
@@ -0,0 +1,15 @@
+class Solution{
+	public:
+
+    vector <int> bellman_ford(int V, vector<vector<int>> adj, int S) {
+        vector<int> dis(V, 100000000);
+        dis[S] = 0;
+        for(int i = 1; i < V; i++){
+            for(auto it: adj){
+                if(dis[it[0]] + it[2] < dis[it[1]])
+                    dis[it[1]] = dis[it[0]] + it[2];
+            }
+        }
+        return dis;
+    }
+};
