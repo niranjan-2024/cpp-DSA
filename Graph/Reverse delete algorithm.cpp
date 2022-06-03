d608c7bf995aa797b46cd417305e3d45fb3fdde5
niranjan-2024 <89894972+niranjan-2024@users.noreply.github.com>
2022-06-03 14:54:02 +0530

    Update Reverse delete algorithm.cpp

--- Graph/Reverse delete algorithm.cpp
+++ Graph/Reverse delete algorithm.cpp
@@ -1,83 +1,81 @@
-struct Node{
-    int u;
-    int v;
-    int wt;
-    Node(int u, int v, int wt){
-        this->u=u;
-        this->v=v;
-        this->wt=wt;
-    }
+class Graph
+{
+    int V;
+    list<int> *adj;
+    vector< pair<int, pair<int, int>> > edges;
+    void DFS(int v, bool visited[]);
+ 
+public:
+    Graph(int V);
+    void addEdge(int u, int v, int w);
+    bool isConnected();
+    int reverseDeleteMST();
 };
 
-class ReverseAlgorithm{
+Graph::Graph(int V)
+{
+    this->V = V;
+    adj = new list<int>[V];
+}
 
-    private:
-        int V;
-        list<int>* adj;
-        vector<Node> edges;
-        
-    public:
-    
-        ReverseAlgorithm(int V){
-            this->V=V;
-            adj=new list<int>[V];
-        }
-        
-        static bool cmp(Node &n1, Node &n2){
-            return n1.wt>n2.wt;
-        }
-        
-        void addEdge(int u, int v, int wt){
+void Graph::addEdge(int u, int v, int w)
+{
+    adj[u].push_back(v); // Add w to vâ€™s list.
+    adj[v].push_back(u); // Add w to vâ€™s list.
+    edges.push_back({w, {u, v}});
+}
+ 
+void Graph::DFS(int v, bool visited[])
+{    visited[v] = true;
+    list<int>::iterator i;
+    for (i = adj[v].begin(); i != adj[v].end(); ++i)
+        if (!visited[*i])
+            DFS(*i, visited);
+}
+
+bool Graph::isConnected()
+{
+    bool visited[V];
+    memset(visited, false, sizeof(visited));
+    DFS(0, visited);
+    for (int i=1; i<V; i++)
+        if (visited[i] == false)
+            return false;
+    return true;
+}
+
+int Graph::reverseDeleteMST()
+{
+    sort(edges.begin(), edges.end());
+    int mst_wt = 0;  // Initialize weight of MST 
+    //cout << "Edges in MST\n";
+    for (int i=edges.size()-1; i>=0; i--)
+    {
+        int u = edges[i].second.first;
+        int v = edges[i].second.second;
+        adj[u].remove(v);
+        adj[v].remove(u);
+        if (isConnected() == false)
+        {
             adj[u].push_back(v);
             adj[v].push_back(u);
-            edges.push_back({u,v,wt});
+            //cout << "(" << u << ", " << v << ") \n";
+            mst_wt += edges[i].first;
         }
-        
-        void DFS(int v, vector<bool> &vis){
-            vis[v]=true;
-            for(auto nei: adj[v]){
-                if(vis[nei]==false){
-                    DFS(nei,vis);
-                }
-            }
-        }
-        
-        bool isConnected(){
-            vector<bool> vis(V,false);
-            DFS(0,vis);
-            for(int i=0;i<V;i++){
-                if(vis[i]==false){
-                    return false;
-                }
-            }
-            return true;
-        }
-        
-        int isMSTAlgorithm(){
-            int MST=0;
-            sort(edges.begin(),edges.end(),cmp);
-            for(auto n: edges){
-                int u=n.u;
-                int v=n.v;
-                adj[u].remove(v);
-                adj[v].remove(u);
-                if(isConnected()==false){
-                    adj[u].push_back(v);
-                    adj[v].push_back(u);
-                    MST+=n.wt;
-                }
-            }
-            return MST;
-        }
-};
+    }
+    return mst_wt;
+}
 
 class Solution {
   public:
     int RevDelMST(int Arr[], int V, int E) {
-         ReverseAlgorithm RA(V);
-        for(int i=0;i<3*E;i+=3){
-            RA.addEdge(Arr[i],Arr[i+1],Arr[i+2]);
-        }
-        return RA.isMSTAlgorithm();
+        
+        Graph g(V);
+        
+        for(int i=0; i<3*E; i+=3)
+            g.addEdge(Arr[i], Arr[i+1], Arr[i+2]);
+            
+        return g.reverseDeleteMST();
+            
     }
 };
