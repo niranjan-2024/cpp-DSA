e3dddfd8b5d3c0d218779c80744bdb9bae97f0e8
niranjan-2024 <89894972+niranjan-2024@users.noreply.github.com>
2022-06-12 13:05:54 +0530

    Create Maximum occurring character.cpp

---
+++ strings & character arrays/Maximum occurring character.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+    public:
+    //Function to find the maximum occurring character in a string.
+    char getMaxOccuringChar(string str)
+    {
+        int arr[26] = {0};
+        
+        for(int i=0;i<str.length();i++){
+            char ch = str[i];
+            int number = 0;
+            if(ch>='a' && ch<='z'){
+                number = ch - 'a';
+            }
+            else{
+                number = ch - 'A';
+            }
+            arr[number]++;
+        }
+        
+        int maxi = -1,ans = 0;
+        for(int i=0;i<26;i++){
+            if(maxi<arr[i]){
+                ans = i;
+                maxi = arr[i];
+            }
+        }
+        return 'a' + ans;
+    }
+
+};
