78f3e8bccab3f7f3dc7ac8c795dc694d91554f8a
niranjan-2024 <89894972+niranjan-2024@users.noreply.github.com>
2022-06-13 12:08:19 +0530

    Create Longest Common Subsequence.cpp

---
+++ strings & character arrays/Longest Common Subsequence.cpp
@@ -0,0 +1,11 @@
+int lcs(string X, string Y, int m, int n)
+{
+    if (m == 0 || n == 0)
+        return 0;
+ 
+    if (X[m - 1] == Y[n - 1])
+        return 1 + lcs(X, Y, m - 1, n - 1);
+    else
+        return max(lcs(X, Y, m, n - 1),
+                   lcs(X, Y, m - 1, n));
+}
