a82bf515bd512c83a2857ba3efed581940a9f423
niranjan-2024 <89894972+niranjan-2024@users.noreply.github.com>
2022-06-18 11:34:00 +0530

    Update Rabin-Karp Algorithm.cpp

--- strings & character arrays/Rabin-Karp Algorithm.cpp
+++ strings & character arrays/Rabin-Karp Algorithm.cpp
@@ -1,87 +1,40 @@
-/* Following program is a C++ implementation of Rabin Karp
-Algorithm given in the CLRS book */
-#include <bits/stdc++.h>
-using namespace std;
-
-// d is the number of characters in the input alphabet
-#define d 256
-
-/* pat -> pattern
-	txt -> text
-	q -> A prime number
-*/
-void search(char pat[], char txt[], int q)
+class Solution
 {
-	int M = strlen(pat);
-	int N = strlen(txt);
-	int i, j;
-	int p = 0; // hash value for pattern
-	int t = 0; // hash value for txt
-	int h = 1;
-
-	// The value of h would be "pow(d, M-1)%q"
-	for (i = 0; i < M - 1; i++)
-		h = (h * d) % q;
-
-	// Calculate the hash value of pattern and first
-	// window of text
-	for (i = 0; i < M; i++)
-	{
-		p = (d * p + pat[i]) % q;
-		t = (d * t + txt[i]) % q;
-	}
-
-	// Slide the pattern over text one by one
-	for (i = 0; i <= N - M; i++)
-	{
-
-		// Check the hash values of current window of text
-		// and pattern. If the hash values match then only
-		// check for characters one by one
-		if ( p == t )
-		{
-			/* Check for characters one by one */
-			for (j = 0; j < M; j++)
-			{
-				if (txt[i+j] != pat[j])
-				{
-				break;
-				}
-				
-					
-			}
-
-			// if p == t and pat[0...M-1] = txt[i, i+1, ...i+M-1]
-		
-			if (j == M)
-				cout<<"Pattern found at index "<< i<<endl;
-		}
-
-		// Calculate hash value for next window of text: Remove
-		// leading digit, add trailing digit
-		if ( i < N-M )
-		{
-			t = (d*(t - txt[i]*h) + txt[i+M])%q;
-
-			// We might get negative value of t, converting it
-			// to positive
-			if (t < 0)
-			t = (t + q);
-		}
-	}
-}
-
-/* Driver code */
-int main()
-{
-	char txt[] = "GEEKS FOR GEEKS";
-	char pat[] = "GEEK";
-	
-	//we mod to avoid overflowing of value but we should take as big q as possible to avoid the collison
-	int q =INT_MAX;
-	
-	// Function Call
-	search(pat, txt, q);
-	return 0;
-}
-
+    public:
+        vector <int> search(string pat, string txt)
+        {
+           long long t=0,p=0,h=1;
+           int n=txt.length(), m=pat.length();
+           
+           int q=1000000007; 
+           int d=256;
+           for(int i=1;i<m;i++)
+           h=(h*d)%q;
+           for(int i=0;i<m;i++)
+           {
+               t=(t*d+txt[i])%q;
+               p=(p*d+pat[i])%q;
+           }
+           vector<int> v;
+           for(int i=0;i<=n-m;i++)
+           {
+               if(t==p)
+               {
+                   if(txt.substr(i,m)==pat)
+                   v.push_back(i+1);
+               }
+               if(i<n-m){
+               t=(d*(t-h*txt[i])+txt[i+m])%q;
+               if(t<0)
+               t+=q;
+               }
+           }
+           if(v.size()==0)
+           {
+               v.push_back(-1);
+               return v;
+           }
+           return v;
+        }
+     
+};
