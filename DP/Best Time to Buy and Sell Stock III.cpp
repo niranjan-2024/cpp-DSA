e1a03a4dbf1033fa7b3165808fd062232170da80
niranjan-2024 <89894972+niranjan-2024@users.noreply.github.com>
2022-07-13 20:06:02 +0530

    Update Best Time to Buy and Sell Stock III.cpp

--- DP/Best Time to Buy and Sell Stock III.cpp
+++ DP/Best Time to Buy and Sell Stock III.cpp
@@ -170,3 +170,32 @@ public:
         return solve(prices);
     }
 };
+
+
+
+
+
+
+
+
+
+//Without Dp (simple best time to buy stock problem similar)
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+     
+        int profit1 = 0;
+        int minprice1 = INT_MAX;
+        int profit2 = 0;
+        int minprice2 = INT_MAX;
+        for(int i=0;i<prices.size();i++)
+        {
+            minprice1 = min(minprice1 , prices[i]);
+            profit1 = max(profit1 , prices[i] - minprice1);
+            minprice2 = min(minprice2 , prices[i] - profit1);
+            profit2 = max(profit2, prices[i] - minprice2);
+        }
+        
+        return profit2;    
+    }
+};
