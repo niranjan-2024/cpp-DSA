be2158a3cba091ef875724ff8a8a7850c91b7489
niranjan-2024 <89894972+niranjan-2024@users.noreply.github.com>
2022-07-16 17:16:51 +0530

    Update Longest Palindromic Subsequence.cpp

--- DP/Longest Palindromic Subsequence.cpp
+++ DP/Longest Palindromic Subsequence.cpp
@@ -1,30 +1,91 @@
-class Solution{
-  public:
-    int longestPalinSubseq(string S) {
-        int n = S.length();
-        if(n<=0){
+//Recursive solution
+class Solution {
+public:
+    
+    int solve(string &s,int left,int right){
+        if(left>right){
             return 0;
         }
-        int table[n][n];
-        
-        for(int i=0;i<n;i++){
-            table[i][i] = 1;
+        if(left==right){
+            return 1;
         }
         
-        for(int k=2;k<=n;k++){
-            for(int i=0;i<n-k+1;i++){
-                int j =i+k-1;
-                if(S[i]==S[j] && k==2){
-                    table[i][j] = 2;
-                }
-                else if(S[i] == S[j]){
-                    table[i][j] = table[i+1][j-1] + 2;
+        if(s[left]==s[right]){
+            return 2+solve(s,left+1,right-1);
+        }
+        else{
+            return max(solve(s,left+1,right),solve(s,left,right-1));
+        }
+    }
+    
+    int longestPalindromeSubseq(string s) {
+        int n = s.length();
+        return solve(s,0,n-1);
+    }
+};
+
+
+//Recursion+Memoization
+class Solution {
+public:
+    
+    int solve(string &s,int left,int right,vector<vector<int>> &dp){
+        if(left>right){
+            return 0;
+        }
+        if(left==right){
+            return 1;
+        }
+        if(dp[left][right]!=-1){
+            return dp[left][right];
+        }
+        if(s[left]==s[right]){
+            dp[left][right] = 2+solve(s,left+1,right-1,dp);
+        }
+        else{
+            dp[left][right] = max(solve(s,left+1,right,dp),solve(s,left,right-1,dp));
+        }
+        return dp[left][right];
+    }
+    
+    int longestPalindromeSubseq(string s) {
+        int n = s.length();
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+        return solve(s,0,n-1,dp);
+    }
+};
+
+
+
+//Tabulation
+// it is similar to lowest common subsequence that if we reverese the give string and find LCS of original
+//and reverse then it will be the longeest palindromic subsequence
+class Solution {
+public:
+    
+    int solve(string &s,int n){
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        string r = s;
+        reverse(r.begin(),r.end());
+    
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(s[i-1]==r[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
                 }
                 else{
-                    table[i][j] = max(table[i+1][j],table[i][j-1]);
+                    dp[i][j] = max(dp[i][j-1],dp[i-1][j]);
                 }
             }
         }
-        return table[0][n-1];
+        return dp[n][n];
+    }
+    
+    int longestPalindromeSubseq(string s) {
+        int n = s.length();
+        if(n<2){
+            return n;
+        }
+        return solve(s,n);
     }
 };
